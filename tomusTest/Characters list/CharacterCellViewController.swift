ef23965adc9a21c1c6655c39c81d5aa81bc2fc41
kot-annett <annktv@gmail.com>
2024-10-02 16:16:09 +0200

    update UI for main screen

--- tomusTest/Characters list/CharacterCellViewController.swift
+++ tomusTest/Characters list/CharacterCellViewController.swift
@@ -19,24 +19,17 @@ final class CharacterCellViewController: UITableViewCell {
         return imageView
     }()
     
-    private let genderLabel: UILabel = {
+    private let nameLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 15)
         label.textColor = .black
         return label
     }()
     
-    private let statusLabel: UILabel = {
+    private let genderLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 15, weight: .regular)
         label.textColor = .gray
-        return label
-    }()
-    
-    private let speciesLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 13, weight: .light)
-        label.textColor = .darkGray
         return label
     }()
     
@@ -53,7 +46,7 @@ final class CharacterCellViewController: UITableViewCell {
     private func setupUI() {
         contentView.backgroundColor = .white
         
-        [characterImageView, genderLabel, statusLabel, speciesLabel].forEach {
+        [characterImageView, nameLabel, genderLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview($0)
         }
@@ -64,21 +57,17 @@ final class CharacterCellViewController: UITableViewCell {
             characterImageView.widthAnchor.constraint(equalToConstant: 100),
             characterImageView.heightAnchor.constraint(equalToConstant: 100),
             
-            genderLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20),
-            genderLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20),
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
             
-            statusLabel.topAnchor.constraint(equalTo: genderLabel.bottomAnchor, constant: 10),
-            statusLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20),
-            
-            speciesLabel.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 10),
-            speciesLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20),
+            genderLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            genderLabel.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20)
         ])
     }
     
     func configure(with character: Character) {
-        genderLabel.text = "Gender: \(character.gender)"
-        statusLabel.text = "Status: \(character.status)"
-        speciesLabel.text = "Species: \(character.species)"
+        nameLabel.text = "Gender: \(character.name)"
+        genderLabel.text = "Status: \(character.gender)"
         
         if let imageUrl = URL(string: character.image) {
             characterImageView.kf.setImage(with: imageUrl)
